5336ddcab0d6638f357474b501199e15ce832326
hadavidh <hadavidh@gmail.com>
2025-12-31 20:23:11 +0100

    ce

---
+++ demo/src/test/java/com/example/gematria/GematriaApplication.java
@@ -0,0 +1,11 @@
+package com.example.gematria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GematriaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GematriaApplication.class, args);
+    }
+}
