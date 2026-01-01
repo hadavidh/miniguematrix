3b3d2fe59a6dd174657cbe869754cbcf4407c9e9
hadavidh <hadavidh@gmail.com>
2026-01-01 14:27:00 +0100

    ajout de tests selenium et maj pipeline

---
+++ demo/src/test/java/com/example/gematria/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gematria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo/src/test/java/com/example/gematria/selenium/tests/GematriaFlowSeleniumIT.java
@@ -0,0 +1,54 @@
+package com.example.gematria.selenium.tests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Sleeper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GematriaFlowSeleniumIT {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+    @BeforeEach
+    void setUp() {
+        baseUrl = System.getProperty("baseUrl", "http://localhost:5173");
+
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless=new", "--no-sandbox", "--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) driver.quit();
+    }
+
+    @Test
+    void userCanComputeGematria() {
+        driver.get(baseUrl);
+
+        // adapte les sélecteurs à ton UI (ex: data-testid)
+        WebElement input = driver.findElement(By.xpath("//*[@id=\"root\"]/div/form/label/input"));
+        input.sendKeys("שלום");
+
+        WebElement btn = driver.findElement(By.xpath("//*[@id=\"root\"]/div/form/button"));
+        btn.click();
+
+        WebElement result = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/pre"));
+        assertTrue(result.getText().length() > 0);
+    }
+    
+    
+    @Test
+    void isGuematriaAtbash() {
+        driver.get(baseUrl);
+
+        System.out.println("ceci est juste un autre test fictif");
+
+    }
+}
